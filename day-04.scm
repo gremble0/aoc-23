df3be39ee6a4fbcc053043a161517430b146f4ee
gremble0 <hermanstornes@gmail.com>
2023-12-04 21:36:22 +0100

    Scuffed part 2 day 4

--- day-04.scm
+++ day-04.scm
@@ -7,16 +7,18 @@
            (loop (cons next-line lines)
                  (read-line)))))))
 
-(define (parse-card card)
-  (define (parse-num-sequence rest cur-num acc)
-    (cond ((null? rest)
-           (if (not (string-null? cur-num))
-             (cons (string->number cur-num) acc)
-             acc))
-          ((char-numeric? (car rest))
-           (parse-num-sequence (cdr rest) (string cur-num (car rest)) acc))
-          (else
-            (parse-num-sequence (cdr rest) "" (cons (string->number cur-num) acc)))))
+;; part 1:
+(define (parse-num-sequence rest cur-num acc)
+  (cond ((null? rest)
+         (if (not (string-null? cur-num))
+           (cons (string->number cur-num) acc)
+           acc))
+        ((char-numeric? (car rest))
+         (parse-num-sequence (cdr rest) (string cur-num (car rest)) acc))
+        (else
+          (parse-num-sequence (cdr rest) "" (cons (string->number cur-num) acc)))))
+
+(define (count-wins card)
   (let* ((card-no-intro (cadr ((string-splitter 'delimiter #\:) card)))
          (card-nums ((string-splitter 'delimiter #\|) card-no-intro))
          (card-winning-nums (parse-num-sequence (string->list (car card-nums)) "" '()))
@@ -34,4 +36,25 @@
         (loop (- wins-iter 1)
               (* points 2))))))
 
-(reduce + 0 (map count-points (map parse-card day-04-input)))
+(reduce + 0 (map count-points (map count-wins day-04-input)))
+
+;; part 2:
+(define (count-scratchcards scratchcards)
+  (define (count-wins card)
+    (let* ((card-split ((string-splitter 'delimiter #\:) card))
+           (card-row (string->number (list->string (filter char-numeric? (string->list (car card-split)))))) ;; eww
+           (card-nums ((string-splitter 'delimiter #\|) (cadr card-split)))
+           (card-winning-nums (parse-num-sequence (string->list (car card-nums)) "" '()))
+           (card-my-nums (parse-num-sequence (string->list (cadr card-nums)) "" '())))
+      (let ((wins (length (filter (lambda (x)
+                       (and x (memq x card-winning-nums))) card-my-nums))))
+        (let loop ((wins-iter wins)
+                   (extra-scratchcards wins))
+          (if (= wins-iter 0)
+            extra-scratchcards
+            (loop (- wins-iter 1)
+                  (+ extra-scratchcards
+                     (count-wins (list-ref scratchcards (- (+ card-row wins-iter) 1))))))))))
+  (+ (length scratchcards) (reduce + 0 (map count-wins day-04-input))))
+
+(count-scratchcards day-04-input)
